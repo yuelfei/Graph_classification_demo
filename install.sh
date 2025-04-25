21d2c1ea0ef329c906e44992c8f7ad2687d7e083
Yuefei Wu <yuelfei.wu@gmail.com>
2025-04-25 20:12:12 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+conda install pytorch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 pytorch-cuda=12.1 -c pytorch -c nvidia
+pip install dgl -f https://data.dgl.ai/wheels/torch-2.3/cu121/repo.html
+pip install ruamel.yaml
+pip install pyg-lib -f https://data.pyg.org/whl/torch-2.3.0+cu121.html
+pip install ogb==1.3.6
+pip install matplotlib==3.9.0 torch_geometric==2.5.3 seaborn==0.13.2
+pip install ruamel.yaml==0.17.21 nni==3.0
